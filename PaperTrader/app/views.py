893a4b02df7d3a4fac32e37cd8cd7c0438328f35
Abena Apaw <akapaw@wustl.edu>
2020-04-19 18:29:51 -0400

    Added a registration page

--- PaperTrader/app/views.py
+++ PaperTrader/app/views.py
@@ -5,6 +5,9 @@ Definition of views.
 from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpRequest
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 def home(request):
     """Renders the home page."""
@@ -17,6 +20,17 @@ def home(request):
             'year':datetime.now().year,
         }
     )
+
+def register(request):
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('home')
+    return render(request, 'app/register.html', {'form': form})
 
 def contact(request):
     """Renders the contact page."""
